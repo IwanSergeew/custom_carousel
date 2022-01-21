37dfb579f7787eaace3b08e306a59d0690e44a85
BuildTools <unconfigured@null.spigotmc.org>
2022-01-21 17:52:43 +0200

    bugfix

--- script.js
+++ script.js
@@ -4,30 +4,29 @@ const loopCarousels = (carousel, index) => {
     const prevButton = carousel.querySelector('.carousel_button-left');
     const dotsNav = carousel.querySelector('.carousel_nav');
     const slides = Array.from(track.children);
-    const slideWidth = slides[0].getBoundingClientRect().width;
-
-    // Set Slides Positions
-    slides.forEach((slide, index) => {
-        slide.style.left = slideWidth * index + 'px';
-    });
+    let slideWidth = slides[0].getBoundingClientRect().width;
 
     // Move Slides Right function
     const moveToNextSlide = () => {
         const currentSlide = track.querySelector('.current_slide');
         const nextSlide = currentSlide.nextElementSibling || currentSlide.parentElement.children[0];
-        const currentDot = dotsNav.querySelector('.current_slide');
-        const targetDot = currentDot.nextElementSibling || currentDot.parentElement.children[0];
         moveSlide(currentSlide, nextSlide);
-        updateDots(currentDot, targetDot);
+        if(dotsNav) {
+            const currentDot = dotsNav.querySelector('.current_slide');
+            const targetDot = currentDot.nextElementSibling || currentDot.parentElement.children[0];
+            updateDots(currentDot, targetDot);
+        }
     }
     // Move Slides Left function
     const moveToPrevSlide = () => {
         const currentSlide = track.querySelector('.current_slide');
         const prevSlide = currentSlide.previousElementSibling || currentSlide.parentElement.children[currentSlide.parentElement.childElementCount - 1];
-        const currentDot = dotsNav.querySelector('.current_slide');
-        const targetDot = currentDot.previousElementSibling || currentDot.parentElement.children[currentDot.parentElement.childElementCount - 1];
         moveSlide(currentSlide, prevSlide);
-        updateDots(currentDot, targetDot);
+        if(dotsNav) {
+            const currentDot = dotsNav.querySelector('.current_slide');
+            const targetDot = currentDot.previousElementSibling || currentDot.parentElement.children[currentDot.parentElement.childElementCount - 1];
+            updateDots(currentDot, targetDot);
+        }
     }
     // Move slides on dot click
     const dotNavClick = (e) => {
@@ -52,6 +51,25 @@ const loopCarousels = (carousel, index) => {
         currentDot.classList.remove('current_slide');
         targetDot.classList.add('current_slide');
     }
+
+    // Set Slides Positions
+    const setSlidesPosition = (slide, index) => {
+        slide.style.left = slideWidth * index + 'px';
+    }
+
+    slides.forEach(setSlidesPosition);
+    // Update slides width and position on window resize
+    window.addEventListener("resize", (e) => {
+        slideWidth = slides[0].getBoundingClientRect().width;
+        slides.forEach(setSlidesPosition);
+        const activeSlide = track.querySelector('.current_slide');
+        const targetIndex = Array.from(track.children).findIndex(slide => slide == activeSlide);
+        track.style.transition = '1ms';
+        track.style.transform = 'translateX(-' + slides[targetIndex].style.left + ')';
+        track.ontransitionend = () => {
+            track.style.transition = null;
+        }
+    });
 
     // Check if buttons are set and add click event
     if(nextButton)
