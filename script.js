a8fd5cebafc122a7f6ab133835bd6dc384c7b40a
BuildTools <unconfigured@null.spigotmc.org>
2022-01-21 19:23:15 +0200

    update

--- script.js
+++ script.js
@@ -5,27 +5,54 @@ const loopCarousels = (carousel, index) => {
     const dotsNav = carousel.querySelector('.carousel_nav');
     const slides = Array.from(track.children);
     let slideWidth = slides[0].getBoundingClientRect().width;
+    let transition_speed = carousel.getAttribute('data-transition-speed');
+    track.style.transition = transition_speed;
+
+    // Transitionend events
+    const moveFromLastToFirst = () => {
+        track.removeEventListener('transitionend', moveFromLastToFirst, false);
+        const currentSlide = track.querySelector('.current_slide');
+        track.style.transition = '1ms';
+        moveSlide(currentSlide, currentSlide.parentElement.children[1]);
+        track.addEventListener('transitionend', removeZeroTransition);
+    }
+    const moveFromFirstToLast = () => {
+        console.log(moveFromFirstToLast);
+        track.removeEventListener('transitionend', moveFromFirstToLast, false);
+        const currentSlide = track.querySelector('.current_slide');
+        track.style.transition = '1ms';
+        moveSlide(currentSlide, currentSlide.parentElement.children[currentSlide.parentElement.childElementCount - 2]);
+        track.addEventListener('transitionend', removeZeroTransition);
+    }
+    const removeZeroTransition = () => {
+        track.removeEventListener('transitionend', removeZeroTransition, false);
+        track.style.transition = transition_speed;
+    }
 
     // Move Slides Right function
     const moveToNextSlide = () => {
         const currentSlide = track.querySelector('.current_slide');
-        const nextSlide = currentSlide.nextElementSibling || currentSlide.parentElement.children[0];
-        moveSlide(currentSlide, nextSlide);
-        if(dotsNav) {
-            const currentDot = dotsNav.querySelector('.current_slide');
-            const targetDot = currentDot.nextElementSibling || currentDot.parentElement.children[0];
-            updateDots(currentDot, targetDot);
+        const nextSlide = currentSlide.nextElementSibling;
+        if(nextSlide) {
+            moveSlide(currentSlide, nextSlide);
+            if(dotsNav) {
+                const currentDot = dotsNav.querySelector('.current_slide');
+                const targetDot = currentDot.nextElementSibling || currentDot.parentElement.children[0];
+                updateDots(currentDot, targetDot);
+            }
         }
     }
     // Move Slides Left function
     const moveToPrevSlide = () => {
         const currentSlide = track.querySelector('.current_slide');
-        const prevSlide = currentSlide.previousElementSibling || currentSlide.parentElement.children[currentSlide.parentElement.childElementCount - 1];
-        moveSlide(currentSlide, prevSlide);
-        if(dotsNav) {
-            const currentDot = dotsNav.querySelector('.current_slide');
-            const targetDot = currentDot.previousElementSibling || currentDot.parentElement.children[currentDot.parentElement.childElementCount - 1];
-            updateDots(currentDot, targetDot);
+        const prevSlide = currentSlide.previousElementSibling;
+        if(prevSlide) {
+            moveSlide(currentSlide, prevSlide);
+            if(dotsNav) {
+                const currentDot = dotsNav.querySelector('.current_slide');
+                const targetDot = currentDot.previousElementSibling || currentDot.parentElement.children[currentDot.parentElement.childElementCount - 1];
+                updateDots(currentDot, targetDot);
+            }
         }
     }
     // Move slides on dot click
@@ -45,6 +72,14 @@ const loopCarousels = (carousel, index) => {
         track.style.transform = 'translateX(-' + target.style.left + ')';
         current.classList.remove('current_slide');
         target.classList.add('current_slide');
+
+        // If last/first element move to other end
+        if(!target.nextElementSibling)
+            track.addEventListener('transitionend', moveFromLastToFirst);
+        else if(!target.previousElementSibling) {
+            console.log('Pre - moveFromFirstToLast');
+            track.addEventListener('transitionend', moveFromFirstToLast);
+        }
     }
 
     const updateDots = (currentDot, targetDot) => {
@@ -56,19 +91,19 @@ const loopCarousels = (carousel, index) => {
     const setSlidesPosition = (slide, index) => {
         slide.style.left = slideWidth * index + 'px';
     }
-
     slides.forEach(setSlidesPosition);
+    
     // Update slides width and position on window resize
     window.addEventListener("resize", (e) => {
         slideWidth = slides[0].getBoundingClientRect().width;
         slides.forEach(setSlidesPosition);
         const activeSlide = track.querySelector('.current_slide');
         const targetIndex = Array.from(track.children).findIndex(slide => slide == activeSlide);
+        
+        // Remove transition on page resize and get new current slide position
         track.style.transition = '1ms';
         track.style.transform = 'translateX(-' + slides[targetIndex].style.left + ')';
-        track.ontransitionend = () => {
-            track.style.transition = null;
-        }
+        track.addEventListener('transitionend', removeZeroTransition);
     });
 
     // Check if buttons are set and add click event
@@ -89,8 +124,13 @@ const loopCarousels = (carousel, index) => {
         dotsNav.addEventListener('click', dotNavClick);
     }
 
-    /* let copy = track.children[0].cloneNode(true);
-    track.append(copy); */
+    const copyFirst = track.children[0].cloneNode(true);
+    const copyLast = track.children[track.childElementCount - 1].cloneNode(true);
+    copyFirst.style.left = slideWidth * track.childElementCount + 'px';
+    copyLast.style.left = slideWidth * -1 + 'px';
+    track.append(copyFirst);
+    track.prepend(copyLast);
+
 }
 
 document.querySelectorAll('.carousel').forEach(loopCarousels);
